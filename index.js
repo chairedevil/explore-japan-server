0660eec7862f9d02af574a306cf381d822936c96
Chaiwat Wattayakorn <chairedevil@live.com>
2018-10-25 12:56:07 +0900

    update heroku

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const myConnection = require('express-myconnection')
 const config = require('./config')
 const routes = require('./routes')
 
-const PORT = 3010
+const PORT = process.env.PORT || 5000
 
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -20,4 +20,5 @@ routes(app)
 
 app.listen(PORT, () => {
     console.log('ready server on http://localhost:' + PORT)
+    console.log('rev1')
 })
