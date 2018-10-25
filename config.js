00fcd53c79426c9ce23a3437f54930fc16c622a9
Chaiwat Wattayakorn <chairedevil@live.com>
2018-10-25 15:38:16 +0900

    config database

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 module.exports = {
     secret: 'chai731token',
     dbOptions: {
-        host    : 'localhost',
-        user    : 'root',
-        password    : 'root',
+        host    : 'uc13jynhmkss3nve.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user    : 'qmvnmcjqncm4lype',
+        password    : 'ni5lh7sse3umf3mu',
         portL   : 3306,
-        database    : 'exploreJapan',
-        socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
+        database    : 'ogowsqsl8yumxy90',
+        //socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
     },
     GOOGLE_API_TOKEN: 'AIzaSyD_vlghBt3iKntZq1nVCR6uGVJuCk3hbdE'
 }
