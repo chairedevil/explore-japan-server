749baf4a0a223ada9f6afef16158ce20e56806bc
Chaiwat Wattayakorn <chairedevil@live.com>
2019-02-24 02:24:32 +0900

    FINAL

--- config.js
+++ config.js
@@ -1,16 +1,17 @@
-/*module.exports = {
+module.exports = {
     secret: 'chai731token',
     dbOptions: {
-        host    : 'uc13jynhmkss3nve.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-        user    : 'qmvnmcjqncm4lype',
-        password    : 'ni5lh7sse3umf3mu',
+        host    : 'alv4v3hlsipxnujn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user    : 'gr2hoen7n0jnvi83',
+        password    : 'uaoophtg0gxlahip',
         portL   : 3306,
-        database    : 'ogowsqsl8yumxy90',
+        database    : 'notp0xuysv2c5z51',
         //socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
     },
     GOOGLE_API_TOKEN: 'AIzaSyD_vlghBt3iKntZq1nVCR6uGVJuCk3hbdE'
-}*/
+}
 
+/*
 module.exports = {
     secret: 'chai731token',
     dbOptions: {
@@ -24,3 +25,4 @@ module.exports = {
     GOOGLE_API_TOKEN: 'AIzaSyD_vlghBt3iKntZq1nVCR6uGVJuCk3hbdE',
     openWeatherMapKey: 'b740b864d8b19429f75b2ff432e93214'
 }
+*/
